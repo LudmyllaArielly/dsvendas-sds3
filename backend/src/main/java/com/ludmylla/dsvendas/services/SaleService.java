6feda6bdf4487ef04e11c38aac16f2b731646f08
Ludmylla Arielly <ludmay4@gmail.com>
2021-05-05 14:52:38 -0300

    Pagination

---
+++ backend/src/main/java/com/ludmylla/dsvendas/services/SaleService.java
@@ -0,0 +1,30 @@
+package com.ludmylla.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ludmylla.dsvendas.dto.SaleDto;
+import com.ludmylla.dsvendas.entities.Sale;
+import com.ludmylla.dsvendas.repository.SaleRepository;
+import com.ludmylla.dsvendas.repository.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository saleRepository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDto> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> sale = saleRepository.findAll(pageable);
+		return sale.map(x -> new SaleDto(x));
+	}
+	
+}
